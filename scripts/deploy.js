6f2a0e80b2e541da10b08648c93ef74faa6fdf4c
Henry Moses <henrymoses@henrys-air.attlocal.net>
2022-06-29 23:40:28 -0400

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    //All I did was fund the contract 0.001 ETH like this:
+    const waveContract = await waveContractFactory.deploy({
+      value: hre.ethers.utils.parseEther("0.001"),
+    });
+  
+    await waveContract.deployed();
+  
+    console.log("WavePortal address: ", waveContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
